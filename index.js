079ea3fd568210046ce3c9db8988f857a00a505a
Bryce Johnson <brycepj@gmail.com>
2015-06-26 22:20:33 -0400

    Add support for help/cli options.

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ var args = process.argv.splice(2);
 var cmd = args[0];
 
 // int deps
-
-if (cmd == 'init') {
+if (!cmd || cmd == '--help') {
+  var api = $require('lib/api/index'),
+      msg = $require('lib/utils/messaging');
+  msg.listKeys(api, "Try one of these:");
+} else if (cmd == 'init') {
   $require('lib/api/init')();
 } else {
   var v = $require('lib/validators').cli_input,
